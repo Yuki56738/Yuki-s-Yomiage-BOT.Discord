032700826813171e1bb605204cb6101e65097986
mint <me>
2022-02-19 21:40:12 +0900

    auto commit

--- log.py
+++ log.py
@@ -12,8 +12,3 @@ def log(arg):
     with open("./log/bot.log", "a") as f:
         f.write(str(dt_now) + ": " + str(arg) + "\n")
 
-
-def log(arg):
-    global dt_now
-    with open("./log/bot.log", "a") as f:
-        f.write(str(dt_now) + ": " + str(arg) + "\n")
